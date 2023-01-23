14eb5ad067f92802943f52c3f70b21dd2cc49054
bday-boy <asmith4642@gmail.com>
2023-01-22 17:22:12 -0700

    Added WIP battery_info files

---
+++ src/battery_info.c
@@ -0,0 +1,74 @@
+#include <stdlib.h> // For malloc(), free()
+
+#include "battery_info.h"
+
+BatteryInfo * new_bat_info() {
+    BatteryInfo * bat_info = malloc(sizeof(BatteryInfo));
+
+    bat_info->bfmanager = new_battery_file_manager();
+
+    bat_info->energy_full = bat_energy_full(bat_info->bfmanager);
+    bat_info->energy_now = bat_energy_now(bat_info->bfmanager);
+    bat_info->energy_design = bat_energy_design(bat_info->bfmanager);
+    bat_info->is_charging = bat_is_charging(bat_info->bfmanager);
+
+    update_percent(bat_info);
+    update_health(bat_info);
+
+    bat_info->state_changed = 0;
+
+    return bat_info;
+};
+
+void del_battery_info(BatteryInfo * bat_info) {
+    del_battery_file_manager(bat_info->bfmanager);
+    free(bat_info);
+};
+
+unsigned short update_info(BatteryInfo * bat_info) {
+    // Read new data from battery and AC files
+    double energy_now = bat_energy_now(bat_info->bfmanager);
+    short is_charging = bat_is_charging(bat_info->bfmanager);
+
+    // State has changed when battery energy or charging state is different
+    // from the previous value
+    unsigned short battery_state_changed = (
+        energy_now != bat_info->energy_now
+        || is_charging != bat_info->is_charging
+    );
+
+    if (battery_state_changed) {
+        bat_info->energy_now = energy_now;
+        bat_info->is_charging = is_charging;
+        update_percent(bat_info);
+    }
+
+    bat_info->state_changed = battery_state_changed;
+    return battery_state_changed;
+};
+
+void update_percent(BatteryInfo * bat_info) {
+    if (bat_info->energy_now > 0.0 && bat_info->energy_full > 0.0) {
+        bat_info->percent_now = 100.0
+            * (bat_info->energy_now / bat_info->energy_full);
+    } else {
+        bat_info->percent_now = NO_INFO;
+    }
+};
+
+void update_health(BatteryInfo * bat_info) {
+    if (bat_info->energy_full > 0.0 && bat_info->energy_design > 0.0) {
+        bat_info->battery_health = 100.0
+            * (bat_info->energy_full / bat_info->energy_design);
+    } else {
+        bat_info->battery_health = NO_INFO;
+    }
+};
+
+double get_bat_health(BatteryInfo * bat_info) {
+    // Make sure we got meaningful data from battery info files
+    if (bat_info->energy_full < 0.0 || bat_info->energy_design < 0.0) {
+        return NO_INFO;
+    }
+    return 100.0 * (bat_info->energy_full / bat_info->energy_design);
+};
---
+++ src/battery_info.h
@@ -0,0 +1,33 @@
+#ifndef BATTERY_INFO_H_
+#define BATTERY_INFO_H_
+
+#include "files.h" // For BatteryFileManager
+
+typedef struct BatteryInfo {
+    // Reads data from AC file, BATX files in the /sys/class/power_supply dir
+    BatteryFileManager * bfmanager;
+
+    // Battery state variables read from files
+    double energy_now;
+    double energy_full;
+    double energy_design;
+    short is_charging;
+
+    // Battery state variables computed from the above variables
+    double percent_now;
+    double battery_health;
+
+    // Useful to avoid unnecessary work in the battery tracker
+    unsigned short state_changed;
+} BatteryInfo;
+
+BatteryInfo * new_bat_info();
+void del_battery_info(BatteryInfo * bat_info);
+
+unsigned short update_info(BatteryInfo * bat_info);
+void update_percent(BatteryInfo * bat_info);
+void update_health(BatteryInfo * bat_info);
+
+double get_bat_health(BatteryInfo * bat_info);
+
+#endif // BATTERY_INFO_H_
